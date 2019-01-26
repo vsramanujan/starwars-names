336b5599eb5b3cf0f699934f420248dd8b547ea0
rveeravallisevil <rveeravallisevil@paypal.com>
2019-01-26 20:39:13 +0530

    Test and Travis

---
+++ src/index.test.js
@@ -0,0 +1,23 @@
+const starWars = require('./index');
+const expect = require('chai').expect;
+
+describe('starwars-names', () => {
+   
+    describe('all', () => {
+        it('should be an array of strings', () => {
+            const isArrayOfStrings = (array) => array.every(item => typeof item === 'string')
+            expect(starWars.all).to.satisfy(isArrayOfStrings);
+        })
+        it('should contain Luke Skywalker', () => {
+            expect(starWars.all).to.include('Luke Skywalker');
+        })
+    });
+
+    describe('random', () => {
+        it('should return a random item from statwars.all', () => {
+            let randomItem = starWars.random();
+            expect(starWars.all).to.include(randomItem);
+        })
+    })
+
+})
